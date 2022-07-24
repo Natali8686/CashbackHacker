55e0f8cd291b8d52a134ea4008bd73aa1b41564c
Наталья Бачурина <natali.gorobets.86@mail.ru>
2022-07-24 22:09:09 +0700

    saving changes

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,4 +25,24 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected,result);
     }
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int amount = 900;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
 }
